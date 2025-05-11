8b027a1b2d1741cf6b180392f74ed376eab127bf
Andras Varga <vargan89@gmail.com>
2025-05-11 23:26:33 +0800

    removed framer-motion from projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import ProjectCard from "./ProjectCard";
 import projectsData from "../data/projectsData";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
@@ -8,13 +8,7 @@ const Projects = () => {
       id="projects"
       className="min-h-screen px-6 py-16 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200"
     >
-      <motion.div
-        initial={{ opacity: 0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut" }}
-        viewport={{ once: true, amount: 0.3 }}
-        className="max-w-6xl mx-auto"
-      >
+      <div className="max-w-6xl mx-auto">
         <h2 className="text-3xl font-bold mb-10 text-center">PROJECTS</h2>
 
         <div className="grid gap-10 grid-cols-1 md:grid-cols-2 place-items-center">
@@ -33,7 +27,7 @@ const Projects = () => {
             </div>
           ))}
         </div>
-      </motion.div>
+      </div>
     </section>
   );
 };
