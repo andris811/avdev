365bae2eb34fce12e94f8571cfe9fb39470fdc9e
Andras Varga <vargan89@gmail.com>
2026-04-23 14:57:03 +0800

    fixed bug with react routing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Introduction from "../components/Introduction";
 import About from "../components/About";
 import Projects from "../components/Projects";
@@ -6,6 +6,20 @@ import Contact from "../components/Contact";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 
 const HomePage = () => {
+  useEffect(() => {
+    const hash = window.location.hash.slice(1);
+    if (hash) {
+      setTimeout(() => {
+        const element = document.getElementById(hash);
+        if (element) {
+          const offset = -80;
+          const position = element.getBoundingClientRect().top + window.pageYOffset;
+          window.scrollTo({ top: position + offset, behavior: "smooth" });
+        }
+      }, 100);
+    }
+  }, []);
+
   return (
     <main className="md:pt-20">
       <Introduction />
